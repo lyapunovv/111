fc6b670e8010e0c22500a59068c5dd922b81bcd9
v.lyapunov <v.lyapunov.exceedteam@gmail.com>
2021-08-02 12:20:52 +0300

    search-stringCompleted

--- 0.Basic/2) search-string/index.js
+++ 0.Basic/2) search-string/index.js
@@ -1,5 +1,17 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+ let value;
+ let result=String("?");
+  for (let key in obj){
+    if(obj[key]===""||typeof obj[key]=="null"||obj[key]==undefined){
+      continue;
+    }
+    value=String(obj[key]);
+    key=String(key);
+    result=(result+key+"="+value+"&");
+    continue;
+  }
+  result=result.slice(0,-1);
+  return result;
 }
 
 window.searchString = searchString;
