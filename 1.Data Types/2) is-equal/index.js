36d2efaec71ec192e10578e9ab5ff76f2fa00137
v.lyapunov <v.lyapunov.exceedteam@gmail.com>
2021-08-05 13:52:53 +0300

    is-equalCompleted

--- 1.Data Types/2) is-equal/index.js
+++ 1.Data Types/2) is-equal/index.js
@@ -1,7 +1,52 @@
-function isEqual(a, b) {
-  // Напиши свой код здесь
+function isEqual(obja, objb) {
+  if (!obja || !objb) {
+    return false;
+  }
+  if (typeof obja !== 'object'|| typeof objb !== 'object'){
+    return false;
+  }
+  
+  let objaKeys=Object.keys(obja).sort();
+  let objbKeys=Object.keys(objb).sort();
+  
+  if(objaKeys.length !== objbKeys.length){
+    return false;
+  }
+
+  const sameKeys = objaKeys.every(key => objbKeys.includes(key));
+
+  if (!sameKeys) {
+    return false;
+  }
+
+  return objaKeys.every(key => {
+    const objAKeyValue = obja[key];
+    const objBKeyValue = objb[key];
+    console.log('1',objaKeys.length)
+    console.log('2', objAKeyValue)
+    if (
+      (!!objAKeyValue && objAKeyValue !== 0 && typeof objAKeyValue === 'object')
+      && (!!objBKeyValue && objBKeyValue !== 0 && typeof objBKeyValue === 'object')
+    ) {
+      const equal = isEqual(objAKeyValue, objBKeyValue);
+      console.log(equal)
+      return equal;
+    } else {
+      return objAKeyValue === objBKeyValue;
+    }
+  })
 }
 
 window.isEqual = isEqual;
 
 export default isEqual;
+
+var a = {
+  a:1,
+  b:1
+}
+
+var b = {
+  b:1,
+  a:1
+}
